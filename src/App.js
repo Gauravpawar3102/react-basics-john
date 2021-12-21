cba687b4b30e3108b13c0328778d019d9b0a02a3
Gaurav <gauravpawar3102@gmail.com>
2021-12-21 16:09:14 +0530

    Self

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return <h1>Shut Up</h1>;
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
 }
 
+function App() {
+  return (
+    <section>
+      <h1>Hey </h1>
+      <Greeting />
+      <h1>ThankYou</h1>
+    </section>
+  );
+}
+
+const Person = () => <h2>John janardhan </h2>;
+const Message = () => (
+  <h2>Hey It's Me John janardhan With a bottle of Water</h2>
+);
 export default App;
