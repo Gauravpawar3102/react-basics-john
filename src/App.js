fff88bef8d19fb894e39842725e36800bc62a551
Gaurav <gauravpawar3102@gmail.com>
2021-12-21 21:08:29 +0530

    first imp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import './App.css';
+// import './App.css';
+// const bookImage = () => {
+//   return (
+//     <section>
+//       <img
+//         src="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym9va3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+//         alt="img"
+//       />
+//     </section>
+//   );
+// };
 
-function Greeting() {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  );
-}
+// function coverPage() {
+//   return (
+//     <section>
+//       <h1>Hey </h1>
+//       <bookImage />
+//       <h1>ThankYou</h1>
+//     </section>
+//   );
+// }
 
-function App() {
-  return (
-    <section>
-      <h1>Hey </h1>
-      <Greeting />
-      <h1>ThankYou</h1>
-    </section>
-  );
-}
-
-const Person = () => <h2>John janardhan </h2>;
-const Message = () => (
-  <h2>Hey It's Me John janardhan With a bottle of Water</h2>
-);
-export default App;
+// export default coverPage;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import coverPage from './App';
+
+function BookList() {
+  return (
+    <section className="bookList">
+      <article className="bookContainer">
+        <Book />
+        <AuthorDetails />
+        <BookDetails />
+      </article>
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <img
+        className="bookImg"
+        src="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym9va3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+        alt="book"
+      />
+    </article>
+  );
+};
+
+const BookDetails = () => {
+  return (
+    <article>
+      <p className="publisher">Board Book</p>
+      <h3 className="price">$5.00</h3>
+    </article>
+  );
+};
+
+const AuthorDetails = () => {
+  return (
+    <article>
+      <h2 className="bookName">First Little Readers</h2>
+      <h3 className="authorName">amelia Hepworth</h3>
+    </article>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BookList />
+    {/* <BookList /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
